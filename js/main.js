dd9d198819e68db6d7c0fa5c2f05a138230892d0
Eli Gundry <eligundry@gmail.com>
2013-02-02 02:10:38 -0500

    Added json parsing library

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
-
+$(document).ready(function() {
+	$.ajax({
+		url: 'http://reddit.com/new.json',
+		method: 'GET',
+		dataType: 'jsonp',
+		done: function(a) {
+			console.log(jQuery.parseJSON(a));
+		}
+	});
+});
